1359095b1a05dbf03df72eda8fb4a9170e893b7b
Gaurav <gauravshevkar>
2025-09-21 17:30:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Reset from './assets/reset'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Reset/> 
+    </>
+  )
+}
+
+export default App
